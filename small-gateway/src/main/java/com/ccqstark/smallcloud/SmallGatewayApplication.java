d24c69ab77016fad507d97baeaabb6017099ca09
ccqstark <1367305698@qq.com>
2021-07-05 02:43:31 +0800

    gateway

---
+++ small-gateway/src/main/java/com/ccqstark/smallcloud/SmallGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ccqstark.smallcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallGatewayApplication.class, args);
+    }
+
+}
